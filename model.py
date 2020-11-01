05accf4d310ddbcae4ed9d568752d6e7d0fb21a1
Ji TANG <ji.tang@polytechnique.edu>
2020-11-01 09:36:51 +0100

    support for tensorflow 2.0

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 import numpy as np
 
 class Model:
@@ -9,7 +10,7 @@ class Model:
         rnn_cells = tf.nn.rnn_cell.MultiRNNCell([lstm_cell(size_layer) for _ in range(num_layers)], state_is_tuple = False)
         self.X = tf.placeholder(tf.float32, (None, None, size))
         self.Y = tf.placeholder(tf.float32, (None, output_size))
-        drop = tf.contrib.rnn.DropoutWrapper(rnn_cells, output_keep_prob = forget_bias)
+        drop = tf.compat.v1.nn.rnn_cell.DropoutWrapper(rnn_cells, output_keep_prob = forget_bias)
         self.hidden_layer = tf.placeholder(tf.float32, (None, num_layers * 2 * size_layer))
         self.outputs, self.last_state = tf.nn.dynamic_rnn(drop, self.X, initial_state = self.hidden_layer, dtype = tf.float32)
         rnn_W = tf.Variable(tf.random_normal((size_layer, output_size)))
